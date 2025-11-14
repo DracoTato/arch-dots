1287f5c716d46d1dae695fc4220150cc477cbd51
dracotato <160530153+DracoTato@users.noreply.github.com>
2025-11-14 20:54:03 +0200

    feat(nvim): steal some neat options and keybinds from kickstart

--- .config/nvim/lua/dracotato/autocmds.lua
+++ .config/nvim/lua/dracotato/autocmds.lua
@@ -1,29 +1,7 @@
--- vim.api.nvim_create_autocmd("LspAttach", {
--- 	callback = function(args)
--- 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
--- 		if not client then
--- 			return
--- 		end
--- 		if client:supports_method("textDocument/formatting") then
--- 			vim.api.nvim_create_autocmd("BufWritePre", {
--- 				buffer = args.buf,
--- 				callback = function()
--- 					-- vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
--- 				end,
--- 			})
--- 		end
--- 	end,
--- })
-
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = "*",
---   callback = function(args)
---     require("conform").format({ bufnr = args.buf })
---   end,
--- })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.hl.on_yank({ timeout = 200 })
-	end,
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank({ timeout = 200 })
+  end,
 })
